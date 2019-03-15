673d26d46782316d78b8002fc9654ecc87c1bd8d
Thorben Jensen <jensen.thorben@gmail.com>
2019-03-15 23:44:34 +0100

    improving architecture of super resolution autoencoder

--- autoencoders/super_res_autoencoder.py
+++ autoencoders/super_res_autoencoder.py
@@ -1,4 +1,4 @@
-"""Super resolution CNN"""
+"""Super resolution autoencoder."""
 
 from datetime import datetime
 
@@ -8,14 +8,17 @@ import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 import tensorflow as tf
 from tensorflow.python.keras.callbacks import TensorBoard
-from tensorflow.python.keras.layers import Conv2D, Input
+from tensorflow.python.keras.layers import Conv2D, Input, BatchNormalization,\
+    concatenate
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.optimizers import Adam
 
 SIGMA = 3
 MIN_RES = (4, 4)
 
-BATCH_SIZE = 32
+BATCH_SIZE = 64
+EPOCHS = 20
+LR = 0.001
 
 # %% loading data
 mnist = tf.keras.datasets.mnist
@@ -65,18 +68,35 @@ plt.show()
 # %% model architecture
 inputs = Input(shape=(28, 28, 1))
 
-h = Conv2D(16, (3, 3), activation='relu', padding='same')(inputs)
-h = Conv2D(8, (3, 3), activation='relu', padding='same')(h)
-h = Conv2D(8, (3, 3), activation='relu', padding='same')(h)
-h = Conv2D(8, (3, 3), activation='relu', padding='same')(h)
-h = Conv2D(4, (3, 3), activation='relu', padding='same')(h)
-h = Conv2D(4, (3, 3), activation='relu', padding='same')(h)
+e1 = Conv2D(8, (3, 3), activation='relu', padding='same')(inputs)
+e1 = BatchNormalization()(e1)
 
-outputs = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(h)
+e2 = Conv2D(16, (3, 3), activation='relu', padding='same')(e1)
+e2 = BatchNormalization()(e2)
+
+e3 = Conv2D(32, (3, 3), activation='relu', padding='same')(e2)
+e3 = BatchNormalization()(e3)
+
+e4 = Conv2D(64, (3, 3), activation='relu', padding='same')(e3)
+e4 = BatchNormalization()(e4)
+
+d1 = Conv2D(32, (3, 3), activation='relu', padding='same')(e4)
+d1 = BatchNormalization()(d1)
+m1 = concatenate([e3, d1], axis=3)
+
+d2 = Conv2D(16, (3, 3), activation='relu', padding='same')(m1)
+d2 = BatchNormalization()(d2)
+m2 = concatenate([e2, d2], axis=3)
+
+d3 = Conv2D(8, (3, 3), activation='relu', padding='same')(m2)
+d3 = BatchNormalization()(d3)
+m3 = concatenate([e1, d3], axis=3)
+
+outputs = Conv2D(1, (3, 3), activation='relu', padding='same')(m3)
 
 # %% Compile model
 model = Model(inputs=inputs, outputs=outputs)
-model.compile(optimizer=Adam(lr=0.0001), loss='mse')
+model.compile(optimizer=Adam(lr=LR), loss='mse')
 print(model.summary())
 
 # %% train model
@@ -86,7 +106,7 @@ callbacks = [
 ]
 
 model.fit(x_train_blur, x_train_norm, batch_size=BATCH_SIZE,
-          epochs=40, verbose=2, validation_split=0.02, callbacks=callbacks)
+          epochs=EPOCHS, verbose=2, validation_split=0.02, callbacks=callbacks)
 
 # %% predict for new data
 x_pred = model.predict(x_test_blur).squeeze()
